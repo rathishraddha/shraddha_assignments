8dfa493867ed73636cf4a58a87411eeb3711b131
rathishraddha <shraddharathi2000@gmail.com>
2024-03-18 15:50:13 +0530

    Logic building assignments

---
+++ Logic Building Assignment/day 5 assignment/Solution.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        // Read the input values
+        String S = scan.nextLine();
+        double d = scan.nextDouble();
+        int i = scan.nextInt();
+        
+        // Print the values with the required format
+        System.out.println("String: " + S);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
+}
